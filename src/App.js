d7167ad14318e4277f6d6dc9a5c12d6d4baae5e9
Sam Curr <sam.k.curr@gmail.com>
2022-10-02 18:29:54 -0600

    About Page and Featured Partners

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
+import AboutPage from './pages/AboutPage';
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage/>} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
             </Routes>
             <Footer />
         </div>
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,14 +4,16 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
-
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
+};
+
+export const selectCommentsByCampsiteId = (campsiteId) => {
+    return COMMENTS.filter(
+        (comment) => comment.campsiteId === parseInt(campsiteId)
+    );
 };
